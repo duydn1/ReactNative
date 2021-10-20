968a9f415525fae99dfb5454b67e5c1cfacce62c
KHPC-P10-DNDUY\duydn1 <duydn1@cpc.vn>
2021-10-20 04:59:08 +0100

    Add Bai Tap 02

--- MyApp/App.tsx
+++ MyApp/App.tsx
@@ -1,114 +1,115 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
-  Text,
-  useColorScheme,
+  SafeAreaView,
   View,
+  Text,
+  TouchableOpacity,
+  TextInput,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <View style={styles.textWrapper}>
+          <Text style={styles.logoText}>Yolo System</Text>
         </View>
-      </ScrollView>
+        <View style={styles.form}>
+          <TextInput
+            style={styles.inputUsername}
+            secureTextEntry={true}
+            autoFocus={true}
+            placeholder="Nhập tên đăng nhập"
+            placeholderTextColor="#929292"
+          />
+          <TextInput
+            style={styles.inputPassword}
+            keyboardType="numeric"
+            secureTextEntry={true}
+            autoFocus={true}
+            placeholder="Nhập mật khẩu"
+            placeholderTextColor="#929292"
+          />
+          <TouchableOpacity 
+            style={[styles.buttonShape, {backgroundColor: 'pink'}]}>
+            <Text style={styles.buttonText}>Login</Text>
+          </TouchableOpacity>
+          <Text style={styles.logoText}>Or</Text>
+          <TouchableOpacity 
+            style={[styles.buttonShape, {backgroundColor: 'blue'}]}>
+            <Text style={styles.buttonText}>Facebook</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.buttonShape, {backgroundColor: 'red'}]}>
+            <Text style={styles.buttonText}>Google</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
-
+const TEXT = {
+  color: 'black',
+  textAlign: 'center',
+  fontSize: 20,
+};
+const INPUT = {
+  height: 60,
+  borderRadius: 30,
+  paddingHorizontal: 30,
+  borderColor: 'black',
+  borderWidth: 2,
+  fontSize: 20,
+  color: '#929292',
+  backgroundColor: '#fff',
+  textAlign: 'center',
+  textAlignVertical: 'center',
+  marginTop: 15,
+};
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: 10,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  content: {
+    paddingHorizontal: 30,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  textWrapper: {
+    marginTop: 60,
+    marginBottom: 30,
   },
-  highlight: {
-    fontWeight: '700',
+  logoText: {
+    textAlign: 'center',
+    color: 'green',
+    fontSize: 50,
+    lineHeight: 50,
+    fontWeight: 'bold',
+    marginTop: 15,
+  },
+  form: {
+    marginBottom: 30,
+  },
+  inputUsername: {
+    ...INPUT,
+  },
+  inputPassword: {
+    ...INPUT,
+  },
+  buttonShape: {
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: '#8d015a',
+    justifyContent: 'center',
+    marginTop: 15,
+  },
+  buttonText: {
+    ...TEXT,
+  },
+  action: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
 });
 
