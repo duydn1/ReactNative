c1879aecd4a07b00cd745215daf9c3ea631afede
KHPC-P10-DNDUY\duydn1 <duydn1@cpc.vn>
2021-10-20 05:50:11 +0100

    Add Screen LoginScreen

--- MyApp/src/screens/LoginScreen/index.tsx
+++ MyApp/src/screens/LoginScreen/index.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import {
   SafeAreaView,
-  StyleSheet,
   View,
   Text,
   TouchableOpacity,
   TextInput,
 } from 'react-native';
-//import {LoginScreen} from './src/screens/LoginScreen/index';
+import {styles} from './styles';
+interface LoginScreenProps {
+  children: ReactNode;
+}
 
-const App = () => {
+function LoginScreen({children}: LoginScreenProps) {
   return (
-    //<View> <LoginScreen />  </View>
     <SafeAreaView style={styles.container}>
       <View style={styles.content}>
         <View style={styles.textWrapper}>
@@ -50,70 +51,6 @@ const App = () => {
       </View>
     </SafeAreaView>
   );
-};
-const TEXT = {
-  color: 'black',
-  textAlign: 'center',
-  fontSize: 20,
-};
-const INPUT = {
-  height: 60,
-  borderRadius: 30,
-  paddingHorizontal: 30,
-  borderColor: 'black',
-  borderWidth: 2,
-  fontSize: 20,
-  color: '#929292',
-  backgroundColor: '#fff',
-  textAlign: 'center',
-  textAlignVertical: 'center',
-  marginTop: 15,
-};
-//export const Container = styled.View``;
-export const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: 10,
-  },
-  content: {
-    paddingHorizontal: 30,
-  },
-  textWrapper: {
-    marginTop: 60,
-    marginBottom: 30,
-  },
-  logoText: {
-    textAlign: 'center',
-    color: 'green',
-    fontSize: 50,
-    lineHeight: 50,
-    fontWeight: 'bold',
-    marginTop: 15,
-  },
-  form: {
-    marginBottom: 30,
-  },
-  inputUsername: {
-    ...INPUT,
-  },
-  inputPassword: {
-    ...INPUT,
-  },
-  buttonShape: {
-    height: 50,
-    borderRadius: 25,
-    backgroundColor: '#8d015a',
-    justifyContent: 'center',
-    marginTop: 15,
-  },
-  buttonText: {
-    ...TEXT,
-  },
-  action: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-});
+}
 
-export default App;
+export default LoginScreen;
